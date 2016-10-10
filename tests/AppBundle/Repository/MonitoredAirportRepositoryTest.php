6fa1c7cfc6384c7035d9b95bbea739495021bf4c
JohnieWalker <deniss.kozickis@gmail.com>
2016-10-10 16:38:11 +0400

    Test coverage iteration 3

--- tests/AppBundle/Repository/MonitoredAirportRepositoryTest.php
+++ tests/AppBundle/Repository/MonitoredAirportRepositoryTest.php
@@ -16,8 +16,30 @@ class MonitoredAirportRepositoryTest extends WebTestCase
     public static function dataProvider()
     {
         return array(
-            array('season' => 1, 'dql' => "SELECT a FROM AppBundle\Entity\MonitoredAirport a WHERE (a.rawMetarDateTime < :datetime OR a.rawMetarDateTime IS NULL) AND a.activeSummer = 1 AND a.alternateSummer = 0" ),
-            array('season' => 0, 'dql' => "SELECT a FROM AppBundle\Entity\MonitoredAirport a WHERE (a.rawMetarDateTime < :datetime OR a.rawMetarDateTime IS NULL) AND a.activeWinter = 1 AND a.alternateWinter = 0")
+            array(
+                'season' => 1,
+                'dql' => "SELECT a FROM AppBundle\Entity\MonitoredAirport a WHERE (a.rawMetarDateTime < :datetime OR a.rawMetarDateTime IS NULL) AND a.activeSummer = 1 AND a.alternateSummer = 0"
+            ),
+            array(
+                'season' => 0,
+                'dql' => "SELECT a FROM AppBundle\Entity\MonitoredAirport a WHERE (a.rawMetarDateTime < :datetime OR a.rawMetarDateTime IS NULL) AND a.activeWinter = 1 AND a.alternateWinter = 0"
+            )
+        );
+    }
+
+    public static function activeDataProvider()
+    {
+        return array(
+            array(
+                'alternate' => 0,
+                'season' => 0,
+                'dql' => "SELECT a FROM AppBundle\Entity\MonitoredAirport a WHERE a.activeWinter = 1 AND a.alternateWinter = :alternate"
+            ),
+            array(
+                'alternate' => 0,
+                'season' => 1,
+                'dql' => "SELECT a FROM AppBundle\Entity\MonitoredAirport a WHERE a.activeSummer = 1 AND a.alternateSummer = :alternate"
+            ),
         );
     }
 
@@ -47,8 +69,28 @@ class MonitoredAirportRepositoryTest extends WebTestCase
         );
 
         $this->assertEquals(
-            $dql, $query->getDQL()
+            $dql,
+            $query->getDQL()
         );
 
+    }
+
+    /**
+     * @dataProvider activeDataProvider
+     */
+    public function testActiveAirports($alternate, $season, $dql)
+    {
+        /**
+         * @var $query \Doctrine\ORM\Query
+         */
+        $query = $this->em->getRepository('AppBundle:MonitoredAirport')->getSeasonActiveAirports(
+            $alternate,
+            $season
+        );
+
+        $this->assertEquals(
+            $dql,
+            $query->getDQL()
+        );
     }
 }

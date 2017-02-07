7f1297130c63a9c6c47f898e0ec9ac2f7067db7d
JohnieWalker <deniss.kozickis@gmail.com>
2017-02-07 18:07:30 +0400

    Changes to admin interface

--- src/AppBundle/Admin/AdminController.php
+++ src/AppBundle/Admin/AdminController.php
@@ -45,33 +45,10 @@ class AdminController extends BaseAdminController
         $queryBuilder = $em->createQueryBuilder()
             ->select(array('entity', 'ad'))
             ->from($entityClass, 'entity')
-            ->leftJoin('entity.airportData', 'ad');
-
-        $queryParameters = array();
-        foreach ($searchableFields as $name => $metadata) {
-            $isNumericField = in_array(
-                $metadata['dataType'],
-                array('integer', 'number', 'smallint', 'bigint', 'decimal', 'float')
-            );
-            $isTextField = in_array($metadata['dataType'], array('string', 'text', 'guid'));
-
-            if ($isNumericField && is_numeric($searchQuery)) {
-                $queryBuilder->orWhere(sprintf('entity.%s = :exact_query', $name));
-                // adding '0' turns the string into a numeric value
-                $queryParameters['exact_query'] = 0 + $searchQuery;
-            } elseif ($isTextField) {
-                $queryBuilder->orWhere(sprintf('entity.%s LIKE :fuzzy_query', $name));
-                $queryParameters['fuzzy_query'] = '%'.$searchQuery.'%';
-
-                $queryBuilder->orWhere(sprintf('entity.%s IN (:words_query)', $name));
-                $queryParameters['words_query'] = explode(' ', $searchQuery);
-            }
-        }
-
-        if (0 !== count($queryParameters)) {
-            $queryBuilder->setParameters($queryParameters);
-        }
-
+            ->leftJoin('entity.airportData', 'ad')
+            ->orWhere('ad.airportIcao LIKE :query')
+            ->setParameter('query', '%'.strtolower($searchQuery).'%');
+        
         if (null !== $sortField) {
             if ($sortField == 'airportData') {
                 $queryBuilder->orderBy('ad.airportIcao', $sortDirection);

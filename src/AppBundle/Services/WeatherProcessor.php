4264954709cf93f16eec3750c7f7cee6f4b2afe5
JohnieWalker <deniss.kozickis@gmail.com>
2017-02-09 10:22:41 +0400

    More

--- src/AppBundle/Services/WeatherProcessor.php
+++ src/AppBundle/Services/WeatherProcessor.php
@@ -325,6 +325,8 @@ class WeatherProcessor
     {
         $toReplace = preg_quote($partToColorize);
 
+        print_r("/\s?($toReplace)(\s|$)?/");
+
         return preg_replace(
             "/\s?($toReplace)(\s|$)?/",
             ' <span class="'.$color.'">$1</span> ',
